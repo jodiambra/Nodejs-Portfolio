56a3349120d42b43faab28dc21a8c481e231fedc
jodiambra <jodiambra@gmail.com>
2023-04-03 14:38:56 -0400

    add files

---
+++ src/pages/about.js
@@ -0,0 +1,152 @@
+import AnimatedText from "@/components/AnimatedText";
+import Education from "@/components/Education";
+import Experience from "@/components/Experience";
+import Layout from "@/components/Layout";
+import Skills from "@/components/Skills";
+import TransitionEffect from "@/components/TransitionEffect";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
+import Head from "next/head";
+import Image from "next/image";
+import React, { useEffect, useRef } from "react";
+import profilePic from "../../public/images/profile/headshot 7.jpg";
+
+const AnimatedNumbers = ({ value }) => {
+  const ref = useRef(null);
+
+  const motionValue = useMotionValue(0);
+  const springValue = useSpring(motionValue, { duration: 3000 });
+  const isInView = useInView(ref, { once: true });
+
+  useEffect(() => {
+    if (isInView) {
+      motionValue.set(value);
+    }
+  }, [isInView, value, motionValue]);
+
+  useEffect(() => {
+    springValue.on("change", (latest) => {
+      if (ref.current && latest.toFixed(0) <= value) {
+        ref.current.textContent = latest.toFixed(0);
+      }
+    });
+  }, [springValue, value]);
+
+  return <span ref={ref}></span>;
+};
+
+const about = () => {
+  return (
+    <>
+      <Head>
+      <title>Minimal Portfolio Built with Nextjs | About Page</title>
+        <meta name="description" content="Learn more about CodeBucks, a Next.js developer with a passion for 
+        creating innovative solutions. Discover tips for building a developer portfolio and insights on 
+        full-stack development, front-end development, and back-end development." />
+      </Head>
+      <TransitionEffect />
+      <main className="flex w-full flex-col items-center justify-center dark:text-light">
+        <Layout className="pt-16">
+          <AnimatedText
+            text="Fueling My Purpose: A Journey to Becoming a Data Scientist"
+            className="mb-16 !leading-tight lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8"
+          />
+          <div className="grid w-full grid-cols-8 gap-16 sm:gap-8">
+            <div className="col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">
+                Biography
+              </h2>
+              <p className="font-medium">
+                Hi there! My name is Jacques Diambra Odi, and I'm a data scientist based in Atlanta, GA. I have over
+                8 years of experience in research and development fields, where I honed my skills in critical thinking,
+                problem-solving, and data analysis. My passion for using data to uncover insights and drive decision-making
+                led me to pursue a career in data science, where I can combine my scientific knowledge with advanced
+                analytics techniques.
+              </p>
+
+              <p className="font-medium">
+                With a strong foundation in statistics, programming, and machine learning, I can create models
+                and algorithms that turn raw data into meaningful insights. I am enthusiastic about continuing to
+                build my skills and applying them to real-world challenges.
+              </p>
+
+              <p className="my-4 font-medium">
+                Prior to becoming a data scientist, I worked as a QC Analyst at Arxada, where I analyzed chemical, raw materials,
+                and in-process materials to support compliance with company standards. I was also a Research Associate at RCUOG,
+                where I contributed to leading-edge Areca nut carcinogenicity data and presented my findings at conferences. In addition,
+                I worked as a Research Lab Assistant at the University of Pittsburgh, where I conducted experiments
+                with fruit fly (drosophila) DNA.
+              </p>
+
+              <p className="font-medium">
+                I hold an M.S. in Biology from the University of Guam and a B.S. in Biology from the University of Pittsburgh.
+                I'm also a graduate of Practicum US Data Science Bootcamp, where i further enhanced my skills. When I'm not working,
+                you can find me on Github, Portfolio, or LinkedIn.
+              </p>
+            </div>
+
+            <div
+              className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark
+bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8
+"
+            >
+              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
+              <Image
+                src={profilePic}
+                alt="Codebucks"
+                className="w-full h-auto rounded-2xl"
+                priority
+                sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+              />
+            </div>
+
+            <div className="col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center md:order-3">
+              <div className="flex flex-col items-end justify-center xl:items-center">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
+                  <AnimatedNumbers value={1} />+
+                </span>
+                <h2
+                  className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base
+                xs:text-sm"
+                >
+                  satisfied clients
+                </h2>
+              </div>
+
+              <div className="flex flex-col items-end justify-center xl:items-center">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
+                  <AnimatedNumbers value={30} />+
+                </span>
+                <h2
+                  className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base
+                xs:text-sm"
+                >
+                  projects completed
+                </h2>
+              </div>
+
+              <div className="flex flex-col items-end justify-center xl:items-center">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
+                  <AnimatedNumbers value={1} />+
+                </span>
+                <h2
+                  className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base
+                xs:text-sm"
+                >
+                  years of experience
+                </h2>
+              </div>
+            </div>
+          </div>
+
+          <Skills />
+          <Experience />
+          <Education />
+        </Layout>
+      </main>
+    </>
+  );
+};
+
+export default about;
