b17e041e63433f82b0bea8ba4803f4d1ff317451
jodiambra <jodiambra@gmail.com>
2023-04-04 17:58:17 -0400

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
       <Layout className="py-8 flex items-center justify-between lg:flex-col lg:py-6">
         <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
         <div className="flex items-center lg:py-2">
-          Built With <span className="text-primary dark:text-primaryDark text-2xl px-1">&#9825;</span>
+          Template Built With <span className="text-primary dark:text-primaryDark text-2xl px-1">&#9825;</span>
           by&nbsp;
           <Link
             href="https://devdreaming.com"
@@ -21,7 +21,7 @@ const Footer = () => {
             "
             target={"_blank"}
           >
-            Jacques Diambra-Odi
+            CodeBucks
           </Link>
         </div>
       </Layout>
